4b2454ccfbdda5440da5e6566e5c067d5e81fa18
LukeJansen <lpjansen@live.co.uk>
2018-01-19 03:06:27 +0000

    Alpha Version 3

--- js/data.js
+++ js/data.js
@@ -2,18 +2,6 @@ var item = ""
 var lastRoom = 0
 
 var roomArray = [
-	// {
-	// 	title:'Test',
-	// 	text:"# Even though you can't bring yourself to touch the body you can see, there seems to be three rips in her clothing that look like something was stabbed through them. [sound/static.wav]<br/><br/>Something clicks into place and pieces of last night coming back to you. Amy came over for dinner, her phone kept making these weird noises[sound/tracking.wav]. She kept saying it was just her boss keeping track of her workload. The next thing you see is Amy standing over you with a syringe in her hand. The memory skips again, you have a knife in your hand. Skips again. Amy is on the floor a pool of blood expanding around her. <br/><br/>Coming back to reality, you look down on her corpse the realisation hits you, your heart is pounding. Amy tried to kill you, so you killed her. \n\nThe noise from her phone, you realise it's the same noise from the person on the train. There must be more answers on her phone.",
-	// 	choices:[
-	// 		{
-	// 			text:"Reload",
-	// 			index:0
-	// 		}
-	// 	],
-	// 	item: "",
-	// 	requirement:""
-	// }
 	{
 		title:'Waking Up',
 		text:'Your eyes slowly open, your head is pounding. You have no recollection of the previous nights events. You try to stand up but a sharp pain from your side forces you back to the floor. You move your hand to your side, looking down your clothing is soaked in blood. Looking around you discover you are in a dark alleyway, only illuminated by the neon spill from the street. There is only one way out of the alleyway and next to you is a industrial waste bin.',
