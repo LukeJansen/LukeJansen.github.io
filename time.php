ea16677eee8f3f165bdddfe80e792cb6b6a8925d
LukeJansen <lpjansen@live.co.uk>
2019-10-30 11:30:46 +0000

    Update time.php
    - Fixed PHP Again

--- time.php
+++ time.php
@@ -1,6 +1,6 @@
 <?php
- date_default_timezone_set('US/Eastern');
+ date_default_timezone_set('Europe/London');
  $currenttime = date("m-d-Y H:i:s");
- list($ddd,$ttt) = split(' ',$currenttime);
- echo "$ddd/$ttt\n";
+ list($ddd,$ttt) = explode(' ',$currenttime);
+ echo "$ddd/$ttt";
 ?>
